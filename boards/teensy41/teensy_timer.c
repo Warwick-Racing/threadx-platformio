68a16cb8e69ca868e36126d4413a0978b01d05c2
Joshua Jose <56792649+joshua-jose@users.noreply.github.com>
2023-09-17 21:10:02 +0100

    Fixed minor timer bugs

--- boards/teensy41/teensy_timer.c
+++ boards/teensy41/teensy_timer.c
@@ -1,14 +1,20 @@
 #include <Arduino.h>
 #include <stdint.h>
 
-
 extern void __tx_SysTickHandler();
 
+static void __attribute((naked, noinline)) gpt1_isr() {
+  GPT1_SR |= GPT_SR_OF1; // clear set bit
+  __asm volatile("dsb");
+  __tx_SysTickHandler();
+}
+
 void teensy_setup_timer() {
+  // convert freq in Hz to a time period in us
   uint32_t DELAY_MICROSEC = 1000000 / TX_TIMER_TICKS_PER_SECOND;
 
-  attachInterruptVector(IRQ_GPT1,
-                        &__tx_SysTickHandler); // attach our tick handler
+  // attach our tick handler
+  attachInterruptVector(IRQ_GPT1, &gpt1_isr);
 
   NVIC_SET_PRIORITY(IRQ_GPT1, 255); // set priority
   NVIC_ENABLE_IRQ(IRQ_GPT1);        // enable timer IRQ
